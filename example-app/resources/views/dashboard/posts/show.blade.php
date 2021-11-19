786b1773663c71b85b97d55f067766cb8803989a
mochammadradhi <m.radhiakbar@gmail.com>
2021-11-19 15:33:59 +0700

    Menyelesaikan Tugas Laravel Mengenai Update & Delete Post Video ke 20

--- example-app/resources/views/dashboard/posts/show.blade.php
+++ example-app/resources/views/dashboard/posts/show.blade.php
@@ -12,14 +12,15 @@
           <article class="my-4 mb-4 align-baseline">{!! $post->body !!}</article>
           <p class="card-text blockquote-footer">By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->name}}">{{$post->category->name}}</a></p>
           <a href="/blog" class="btn btn-primary"><span data-feather="arrow-left"></span> Back to My Posts</a>
-          <a href="" class="btn btn-warning">
+          <a href="/dashboard/posts/{{$post->slug}/edit}}" class="btn btn-warning">
             <span data-feather="edit"></span>
             Edit
-          </a>
-          <a href="" class="btn btn-danger">
-            <span data-feather="x-circle"></span>
-            Delete
-          </a>
+          </a>  
+          <form action="/dashboard/posts{{$post->slug}}" method="post" class="d-inline">
+            @method('delete')
+            @csrf
+            <button class="btn bg-danger" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"> Delete</span></button>
+            </form>
       </div>
 </div>
 @endsection
