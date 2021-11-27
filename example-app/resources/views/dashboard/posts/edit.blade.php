4e24ea63be5b492a729d03035bddb4dfd33c4642
mochammadradhi <m.radhiakbar@gmail.com>
2021-11-27 19:30:33 +0700

    Menyelesaikan Tugas Laravel mengenai Preview,Update, & Delete Image Video ke 22

--- example-app/resources/views/dashboard/posts/edit.blade.php
+++ example-app/resources/views/dashboard/posts/edit.blade.php
@@ -7,7 +7,7 @@
     
   </div>
   <div class="col-lg-8">
-    <form method="post" action="/dashboard/posts/{{$post->slug}}">
+    <form method="post" action="/dashboard/posts/{{$post->slug}}" enctype="multipart/form-data">
         @method('put')
         @csrf
         <div class="mb-3">
@@ -44,6 +44,22 @@
         </div>
 
         <div class="mb-3">
+          <label for="formFile" class="form-label">Post Image</label>
+          <input type="hidden" name="oldImage" value="{{ $post->image }}"> 
+          @if ($post->image)
+          <img src="{{asset('storage/' . $post->image )}}" class="img-preview img-fluid mb-3 col-sm-5 d-block" src="" alt="">
+          @else
+          <img class="img-preview img-fluid mb-3 col-sm-5" src="" alt="">
+          @endif
+          <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
+          @error('image')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div>
+                @enderror
+        </div>
+
+        <div class="mb-3">
             <div class="form-group">
                <label for="body">Body</label>
                @error('body')
@@ -72,5 +88,19 @@
     document.addEventListener('trix-file-accept', function(){
         e.preventDefault();
     });
+
+    function previewImage(){
+    const image = document.querySelector('#image')
+    const imgPreview = document.querySelector('.img-preview')
+
+    imgPreview.style.display = 'block';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+
+    oFReader.onload = function(oFREvent){
+      imgPreview.src = oFREvent.target.result
+    }
+    }
   </script>
 @endsection
